b1432317b1c964b88009587dec489fc5e1efaf46
Alex Redmon <alex.redmon@cylera.com>
2019-12-22 13:15:15 -0500

    Body tweaks

--- components/layout/Body/index.js
+++ components/layout/Body/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
+import styles from './styles.css'
+
 
 export const BodyWeb = props => (
   <div
     style={{
       fontFamily: 'helvetica, sans-serif',
-      height: 'calc(100vh - 60px)',
+      height: 'calc(100vh - 80px)',
     }}
   >
     {props.children}
