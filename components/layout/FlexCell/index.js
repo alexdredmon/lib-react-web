1fe4620d2c93bc3dd606f2990f394915ac84217e
Alex Redmon <alex.redmon@cylera.com>
2019-12-22 13:19:43 -0500

    Simplified FlexCell

--- components/layout/FlexCell/index.js
+++ components/layout/FlexCell/index.js
@@ -1,44 +1,17 @@
 import React from 'react'
-import classNames from 'classnames'
 
-import './styles.css'
+export const FlexCellWeb = props => (
+  <div
+    style={{
+      alignItems: 'center',
+      flex: 1,
+      display: 'flex',
+      justifyContent: 'center',
+      flexDirection: 'column',
+    }}
+  >
+    { props.children }
+  </div>
+)
 
-
-const alignMap = {
-  top: 'flex-start',
-  center: 'center',
-  bottom: 'flex-end',
-  left: 'flex-start',
-  right: 'flex-end',
-}
-
-class FlexCell extends React.Component {
-  static defaultProps = {
-    align: 'center',
-    flex: 1,
-    valign: 'center',
-  }
-  
-  render = () => {
-    const { align, children, className, flex, valign } = this.props
-    return (
-      <div
-        className={classNames(
-          'flex-cell',
-          className,
-        )}
-        style={{
-          flex: flex,
-          alignItems: alignMap[valign],
-          justifyContent: align[align],
-        }}
-      >
-        <div style={{ width: '100%', display: 'block'}}>
-          { children }
-        </div>
-      </div>
-    )
-  }
-}
-
-export default FlexCell
+export default FlexCellWeb
