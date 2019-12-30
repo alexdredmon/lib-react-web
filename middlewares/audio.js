4a2eb8730788ecc58deca466e3e8565f41322910
Alex Redmon <alex.redmon@cylera.com>
2019-12-29 20:37:19 -0500

    Audio middleware

--- middlewares/audio.js
+++ middlewares/audio.js
@@ -6,7 +6,12 @@ const webAudioMiddleware = store => next => async action => {
 
   if (action) {
     if (action.type === PLAY_SOUND) {
-
+      var audio = new Audio(action.webSource)
+      if (action.loop) {
+        audio.loop = true
+      }
+      audio.volume = action.volume
+      audio.play();
     }
   }
   return next(action)
